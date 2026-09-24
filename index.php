bebdd410e2336c5d1ee9832a1dbcb863d8371eba
Sala A1 <csalaa1@uniboyaca.edu.co>
2026-09-24 18:39:23 -0500

    Conexion a db

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$host = "localhost";
+$user_db = "root";
+$pwd_db = "";
+$name_db = "usuarios_db";
+
+$conx = new mysqli($host, $user_db, $pwd_db, $name_db);
+
+if ($conx->connect_error) {
+    print($conx->connect_error);
+    die();
+}
+
+echo "Conexion exitosa!!!<br>";
+
+$sql = "insert into usuarios (nombre, username, password)";
+$sql .= "values ('Juan','juan','juan12345')";
+
+$result = $conx->query($sql);
+if ($result) {
+    echo "Insert exitoso!!!<br>";
+}
+
+$conx->close();
